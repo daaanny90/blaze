1e7883709dbec3dd3570347ba6ab4438f91b99c9
Danny Spina <danny@ostec.de>
2023-06-19 00:46:43 +0200

    Forgive me, it's late

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ import { fileURLToPath } from "url";
 const app = express();
 const port = 5763;
 
+// @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
