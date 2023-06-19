d930272730f8862935005002dbdf11a004900a62
Danny Spina <danny.spina@hotmail.it>
2023-06-19 12:05:59 +0200

    Change everything to become a search engine

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import express from "express";
-import { Readability } from "@mozilla/readability";
+import { Readability, isProbablyReaderable } from "@mozilla/readability";
 import { JSDOM } from "jsdom";
 import got from "got";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-const port = 5763;
+const port = 8888;
 
 // @ts-ignore
 const __filename = fileURLToPath(import.meta.url);
@@ -14,36 +14,85 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.get("/", (req, res) => {
-  let url = req.query.at as string;
+  const searchEngine = "https://duckduckgo.com/html/";
+  let query = req.query.q as string;
 
-  if (!url) {
+  if (!query) {
     res.sendFile(path.join(__dirname + "/dist/index.html"));
+    return
   }
 
-  var re = new RegExp("^(http|https)://", "i");
-  var match = re.test(url);
-
-  if (!match) {
-    url = `https://${url}`
-  }
-
-  got(url)
+  got(`${searchEngine}?q=${query}`)
     .then((response) => {
       const dom = new JSDOM(response.body);
-      let reader = new Readability(dom.window.document);
-      let article = reader.parse();
+      const links = dom.window.document.querySelectorAll('link')
+      const results = dom.window.document.querySelectorAll('.result')
 
-      if (!article) {
-        res.send("Something went wrong");
-        return;
-      }
+      // add custom style
+      const style = dom.window.document.createElement('style')
+      dom.window.document.querySelector('head')?.appendChild(style)
+      style.innerHTML = `
+        .result {
+          padding: 1rem;
+          border: 1px solid black;
+          margin-bottom: 10px:
+        }
 
-      res.send(article.content);
+        .result__snippet, .result__url {
+          text-decoration: none;
+          color: black
+        }
+      `
+      // clean up dom
+      links.forEach(linkTag => linkTag.remove())
+      dom.window.document.querySelector('form')?.remove()
+      dom.window.document.querySelector('#header')?.remove()
+
+      // modify urls
+      results.forEach(result => {
+        const hrefArray = result.querySelector('.result__snippet')?.getAttribute('href')?.split('')
+        hrefArray?.splice(0,25)
+        const cleanHref = hrefArray?.join('')
+        const newHref = `http://localhost:8888/blazed?url=${cleanHref}`
+        result.querySelector('.result__snippet')?.setAttribute('href', newHref)
+        result.querySelector('.result__url')?.setAttribute('href', newHref)
+        result.querySelector('.result__a')?.setAttribute('href', newHref)
+      })
+
+      res.send(dom.serialize())
+      return;
     })
     .catch((err) => {
       console.log(err);
     });
 });
+
+app.get("/blazed", (req, res) => {
+const pageToBlaze = req.query.url as string
+ 
+got(pageToBlaze)
+   .then((response) => {
+     const dom = new JSDOM(response.body);
+
+     if (!isProbablyReaderable(dom.window.document)) {
+      res.send("This page can not be blazed... Sorry!")
+      return;
+     }
+
+     let reader = new Readability(dom.window.document);
+     let article = reader.parse();
+
+     if (!article) {
+       res.send("Something went wrong");
+       return;
+     }
+
+     res.send(article.content);
+   })
+   .catch((err) => {
+     console.log(err);
+   }); 
+})
 
 app.listen(port, () => {
   console.log(`Got request`);
